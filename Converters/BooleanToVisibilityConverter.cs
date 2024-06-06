7890062b9bba38745fa38e7475e5fbcb000f1c0f
markk <m.kors@rocmn.nl>
2024-06-06 09:23:52 +0200

    Add project files.

---
+++ Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace WpfApp_060624.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            bool val = (bool)value;
+            if (val == true)
+            {
+                return Visibility.Visible;
+            } else
+            {
+                return Visibility.Collapsed;
+            }
+                
+           
+                
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
